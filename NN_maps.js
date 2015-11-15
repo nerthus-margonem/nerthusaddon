4678214857f9ab018c5ae9d92bea811473859141
akrzyz <aleksander.krzyz@gmail.com>
2015-11-15 22:33:20 +0100

    more clean im maps

--- NN_maps.js
+++ NN_maps.js
@@ -1,6 +1,6 @@
 /**
-	Name: NerthusMaps
-	Zawiera zmiane map na nocne oraz na sezonowe.
+    Name: NerthusMaps
+    Zawiera zmiane map na nocne oraz na sezonowe.
 **/
 try{
 
@@ -8,21 +8,19 @@ nerthus.maps = {}
 
 nerthus.maps.seasonMaps = function()
 {
-    //mapy okresowe - pory roku
     var season = nerthus.season();
-	for(i in nerthus.mapsArr)
-	{
-		if( nerthus.mapsArr[i][0] == season && nerthus.mapsArr[i][1] == map.id )
-		{
-			this.change(nerthus.mapsArr[i][2]);
-		}
-	}
+    for(i in nerthus.mapsArr)
+    {
+        if( nerthus.mapsArr[i][0] == season && nerthus.mapsArr[i][1] == map.id )
+        {
+            this.change(nerthus.mapsArr[i][2]);
+        }
+    }
 }
 
-//zmiana map, nie dotyczny map nocnych bo są w folderze którego gracze nie zasysają :/
 nerthus.maps.change = function(map_url)
 {
-	$("#ground").css("backgroundImage","url(" + map_url + ")");
+    $("#ground").css("backgroundImage","url(" + map_url + ")");
 }
 
 nerthus.maps.seasonMaps();

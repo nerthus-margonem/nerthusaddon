cff8ee941f45b3b6e638a58f39af51805937d44d
akrzyz <aleksander.krzyz@gmail.com>
2015-11-16 22:39:48 +0100

    comment config_hax

--- NN_dlaRadnych.js
+++ NN_dlaRadnych.js
@@ -51,7 +51,7 @@ nerthus.mapsArr =
 //lista dodatkowych skryptów odpalanych w dodatku, tak na przyszłość - dodawać adres skryptu w apostrofach np: 'http://addons.margonem.pl/get/82.js'
 nerthus.additionaScripts =
 [
-    'NN_Config_hax.js',
+//    'NN_Config_hax.js',
     'NN_chatCmd.js',
     'NN_alko.js',
     'NN_NightLights.js'

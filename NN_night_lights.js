b1505f6818e7ddd5f21818abebd75484a9664ec3
akrzyz <aleksander.krzyz@gmail.com>
2015-11-20 22:46:31 +0100

    remove debug log

--- NN_night_lights.js
+++ NN_night_lights.js
@@ -1,17 +1,21 @@
 try
 {
-log("starting night lights")
+
 nerthus.night_lights = {}
+
 nerthus.night_lights.types = {}
+
 nerthus.night_lights.types.add = function(type,size)
 {
     this[type] = {'url' : nerthus.addon.fileUrl("/img/night_light_" + type + ".png"), 'x' : size, 'y' : size, 'type' : type}
 }
+
 nerthus.night_lights.add = function(lights)
 {
     for(var i in lights)
         this.display(this.types[lights[i].type], lights[i].x, lights[i].y)
 }
+
 nerthus.night_lights.display = function(light, x, y)
 {
     return $('<div/>')
@@ -27,12 +31,14 @@ nerthus.night_lights.display = function(light, x, y)
     .attr("type", light.type)
     .appendTo("#ground")
 }
+
 nerthus.night_lights.on = function()
 {
     var hour = new Date().getHours();
     if( hour < 4 || hour > 18 )
         $.getJSON(nerthus.addon.fileUrl("/night_lights/map_" + map.id + ".json"),function(lights){nerthus.night_lights.add(lights)})
 }
+
 nerthus.night_lights.types.add("S","64px")
 nerthus.night_lights.types.add("M","96px")
 nerthus.night_lights.types.add("L","160px")

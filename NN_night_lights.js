d13216928d2303e7220a6f706491d40f3e786628
akrzyz <aleksander.krzyz@gmail.com>
2015-11-20 22:42:59 +0100

    Revert "simplify getJSON handling"
    This reverts commit a2b39307095828be7bb64ff3d0af7871ffb082d8.

--- NN_night_lights.js
+++ NN_night_lights.js
@@ -1,21 +1,17 @@
 try
 {
-
+log("starting night lights")
 nerthus.night_lights = {}
-
 nerthus.night_lights.types = {}
-
 nerthus.night_lights.types.add = function(type,size)
 {
     this[type] = {'url' : nerthus.addon.fileUrl("/img/night_light_" + type + ".png"), 'x' : size, 'y' : size, 'type' : type}
 }
-
 nerthus.night_lights.add = function(lights)
 {
     for(var i in lights)
         this.display(this.types[lights[i].type], lights[i].x, lights[i].y)
 }
-
 nerthus.night_lights.display = function(light, x, y)
 {
     return $('<div/>')
@@ -31,14 +27,12 @@ nerthus.night_lights.display = function(light, x, y)
     .attr("type", light.type)
     .appendTo("#ground")
 }
-
 nerthus.night_lights.on = function()
 {
     var hour = new Date().getHours();
     if( hour < 4 || hour > 18 )
-        $.getJSON(nerthus.addon.fileUrl("/night_lights/map_" + map.id + ".json"), nerthus.night_lights.add)
+        $.getJSON(nerthus.addon.fileUrl("/night_lights/map_" + map.id + ".json"),function(lights){nerthus.night_lights.add(lights)})
 }
-
 nerthus.night_lights.types.add("S","64px")
 nerthus.night_lights.types.add("M","96px")
 nerthus.night_lights.types.add("L","160px")

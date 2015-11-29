3afee54a8044d715332ceb7fcd9ad81ef1572958
akrzyz <aleksander.krzyz@gmail.com>
2015-11-29 11:02:32 +0100

    add start to alko

--- NN_alko.js
+++ NN_alko.js
@@ -3,58 +3,34 @@
 //==================================================================
 try
 {
-nerthus_alk = {};
+nerthus.alko = {};
 
-nerthus_alk.lvl = 0;  //stanupojenia alkoholowego 0 trzeźwy 100 urwany film.
+nerthus.alko.lvl = 0;  //stanupojenia alkoholowego 0 trzeźwy 100 urwany film.
 
-nerthus_alk.timer = 0;
+nerthus.alko.timer = 0;
 
-nerthus_alk.start = function()
+nerthus.alko.run = function()
 {
-    if (getCookie('nerthus_alk') != null) 
-        nerthus_alk.lvl = getCookie('nerthus_alk')
+    if (getCookie('nerthus.alko') != null) 
+        nerthus.alko.lvl = getCookie('nerthus.alko')
     else
-        nerthus_alk.lvl = 0;
-    if (nerthus_alk.lvl) nerthus_alk.timer = setInterval(nerthus_alk.timer_handler, 5000);
+        nerthus.alko.lvl = 0;
+    if (nerthus.alko.lvl)
+        nerthus.alko.timer = setInterval(nerthus.alko.timer_handler, 5000);
 }
 
-nerthus_alk.timer_handler = function()
+nerthus.alko.timer_handler = function()
 {
-    nerthus_alk.lvl--;
+    nerthus.alko.lvl--;
     expiry = new Date(parseInt(new Date().getTime()) * 2);
-    setCookie("nerthus_alk", nerthus_alk.lvl, expiry)
-    if (nerthus_alk.lvl<1) 
+    setCookie("nerthus.alko", nerthus.alko.lvl, expiry)
+    if (nerthus.alko.lvl<1) 
     {
-        nerthus_alk.lvl=0;
-        clearInterval(nerthus_alk.timer);
-        nerthus_alk.timer=0
+        nerthus.alko.lvl=0;
+        clearInterval(nerthus.alko.timer);
+        nerthus.alko.timer=0
     };
 }
-
-// przechwytujemy _g
-_nerthg = _g;
-_g = function (c,d)
-{
-    // jeżli użyjemy towaru konsumpcyjnego o wymaganiach levelowych 18 to dodaje nam 10% upojenia alkoholowego
-    if (c.search("moveitem&st=1&id=") > -1) 
-    {
-        var it = g.item[c.slice(17)];
-        if (it.cl == 16 || it.cl == 23)
-        if (it.stat.search("lvl=")>-1)
-        if (parseInt(it.stat.match(/lvl=([0-9]+)/)[1]) == 18)
-        {
-            nerthus_alk.lvl+= 10;
-            if (nerthus_alk.lvl > 100) nerthus_alk.lvl = 100
-            if (!nerthus_alk.timer) nerthus_alk.timer = setInterval(nerthus_alk.timer_handler,5000);
-        }
-    }
-    _nerthg(c,d);
-}
-
-
-//==================================================================
-//Pijackie gadanie
-//==================================================================
 
 function shuffleArray(array,cc) 
 { // funkcja z netu tasujÄ?ca  tablicÄ? pozostawiajÄ?c 
@@ -71,49 +47,77 @@ function shuffleArray(array,cc)
     return array;
 }
 
-var nerth_chatSendMsg = chatSendMsg;
-chatSendMsg = function (a)
+nerthus.alko.start = function()
 {
-    if ((a[0]!="*") && (a[0]!="/")&& (a[0]!="@") && (nerthus_alk.lvl>0))
+    // przechwytujemy _g
+    var _nerthg = _g;
+    _g = function (c,d)
     {
-        switch (Math.floor(nerthus_alk.lvl/10))
+        // jeżli użyjemy towaru konsumpcyjnego o wymaganiach levelowych 18 to dodaje nam 10% upojenia alkoholowego
+        if (c.search("moveitem&st=1&id=") > -1) 
         {
-        case 9: a = "/me bełkota coś niezrozumiale.";
-            break;
-        case 8:
-            a = shuffleArray(a.split(" ")).join(" ");
-        case 7: 
-            t = a.split(", ");
-            for (tt in t) t[tt]=shuffleArray(t[tt].split(" ")).join(" ");
-            a = t.join(", ");
-        case 6: 
-            t = a.split(", ");
-            for (tt in t) t[tt]=shuffleArray(t[tt].split(" "),1).join(" ");
-            a = t.join(", ");
-        case 5: 
-            t = a.split(" ");
-            for (tt in t) if (t[tt].length > 4) t[tt]=shuffleArray(t[tt].split("")).join("");
-            a = t.join(" ");
-        case 4: 
-            t = a.split(" ");
-            for (tt in t) if (t[tt].length > 5) t[tt]=shuffleArray(t[tt].split("")).join("");
-            a = t.join(" ");
-        case 3: 
-            t = a.split(" ");
-            for (tt in t) if (t[tt].length > 4) t[tt]=shuffleArray(t[tt].split(""),1).join("");
-            a = t.join(" ");
-        case 2: 
-            t = a.split(" ");
-            for (tt in t) if (t[tt].length > 5) t[tt]=shuffleArray(t[tt].split(""),1).join("");
-            a = t.join(" ");
-        case 1:
-            a = a.replace(/\.|\,|\:|\?|\!|\-/g," *hik*");
-        case 0:
-            a = a.replace(/\.|\,|\:|\?|\!|\-/g,"");
-        }  
-    }    
-    nerth_chatSendMsg(a)
-};
+            var it = g.item[c.slice(17)];
+            if (it.cl == 16 || it.cl == 23)
+            if (it.stat.search("lvl=")>-1)
+            if (parseInt(it.stat.match(/lvl=([0-9]+)/)[1]) == 18)
+            {
+                nerthus.alko.lvl+= 10;
+                if (nerthus.alko.lvl > 100)
+                    nerthus.alko.lvl = 100
+                if (!nerthus.alko.timer)
+                    nerthus.alko.timer = setInterval(nerthus.alko.timer_handler,5000);
+            }
+        }
+        _nerthg(c,d);
+    }
 
-g.loadQueue.push({fun:nerthus_alk.start,data:""});
+    var nerth_chatSendMsg = chatSendMsg;
+    chatSendMsg = function (a)
+    {
+        if ((a[0]!="*") && (a[0]!="/")&& (a[0]!="@") && (nerthus.alko.lvl>0))
+        {
+            switch (Math.floor(nerthus.alko.lvl/10))
+            {
+            case 9: a = "/me bełkota coś niezrozumiale.";
+                break;
+            case 8:
+                a = shuffleArray(a.split(" ")).join(" ");
+            case 7: 
+                t = a.split(", ");
+                for (tt in t) t[tt]=shuffleArray(t[tt].split(" ")).join(" ");
+                a = t.join(", ");
+            case 6: 
+                t = a.split(", ");
+                for (tt in t) t[tt]=shuffleArray(t[tt].split(" "),1).join(" ");
+                a = t.join(", ");
+            case 5: 
+                t = a.split(" ");
+                for (tt in t) if (t[tt].length > 4) t[tt]=shuffleArray(t[tt].split("")).join("");
+                a = t.join(" ");
+            case 4: 
+                t = a.split(" ");
+                for (tt in t) if (t[tt].length > 5) t[tt]=shuffleArray(t[tt].split("")).join("");
+                a = t.join(" ");
+            case 3: 
+                t = a.split(" ");
+                for (tt in t) if (t[tt].length > 4) t[tt]=shuffleArray(t[tt].split(""),1).join("");
+                a = t.join(" ");
+            case 2: 
+                t = a.split(" ");
+                for (tt in t) if (t[tt].length > 5) t[tt]=shuffleArray(t[tt].split(""),1).join("");
+                a = t.join(" ");
+            case 1:
+                a = a.replace(/\.|\,|\:|\?|\!|\-/g," *hik*");
+            case 0:
+                a = a.replace(/\.|\,|\:|\?|\!|\-/g,"");
+            }  
+        }    
+        nerth_chatSendMsg(a)
+    };
+
+    nerthus.defer(nerthus.alko.run)
+}
+
+nerthus.alko.start()
+
 }catch(e){log('NerthusAlk Error: '+e.message,1)}

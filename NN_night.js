46ce89d131bb494f55306375f50c218e3d491e5c
akrzyz <aleksander.krzyz@gmail.com>
2015-12-08 21:40:37 +0100

    some this in night

--- NN_night.js
+++ NN_night.js
@@ -61,7 +61,7 @@ nerthus.night.lights.on = function()
 {
     var hour = new Date().getHours();
     if( hour < 4 || hour > 18 )
-        $.getJSON(nerthus.addon.fileUrl("/night_lights/map_" + map.id + ".json"),function(lights){nerthus.night.lights.add(lights)})
+        $.getJSON(nerthus.addon.fileUrl("/night_lights/map_" + map.id + ".json"),this.add.bind(this))
 }
 
 nerthus.night.start = function()
@@ -72,8 +72,8 @@ nerthus.night.start = function()
     this.lights.types.add("XL","192px")
     if(nerthus.options['night'])
     {
-        nerthus.defer(nerthus.night.lights.on)
-        nerthus.defer(nerthus.night.dim)
+        nerthus.defer(this.lights.on.bind(this))
+        nerthus.defer(this.dim.bind(this))
     }
 }
 

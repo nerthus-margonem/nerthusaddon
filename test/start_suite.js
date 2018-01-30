7b28dd42b05e27a58c3bcd5bdccf567d325f1cec
akrzyz <akrzyz>
2018-01-30 20:22:25 +0100

    rm test dummt

--- test/start_suite.js
+++ test/start_suite.js
@@ -10,12 +10,12 @@ beforeEach(function(){
 
 suite("start")
 
-test("default version and prefix",function(){
+test("default version and prefix", function(){
     expect(nerthus.addon.version).to.be.equal('master')
     expect(nerthus.addon.filesPrefix).to.be.equal('http://cdn.rawgit.com/akrzyz/nerthusaddon')
 })
 
-test("fileUrl concat filesPrefix, version and file_name into url",function(){
+test("fileUrl concat filesPrefix, version and file_name into url", function(){
 
     var file = 'SCRIPT.JS'
     var prefix = 'PREFIX'
@@ -27,7 +27,4 @@ test("fileUrl concat filesPrefix, version and file_name into url",function(){
     expect(nerthus.addon.fileUrl(file)).to.be.equal(file_url)
 })
 
-test("dummy", function(){
-    var dummy = true
-    expect(dummy).to.be.ok()
-})
+

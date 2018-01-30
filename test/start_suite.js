21d7a868cb35f20a9cb4de56b251224439f68afd
akrzyz <akrzyz>
2018-01-30 20:08:21 +0100

    add ut for default version and file prefix nad fileUrl(..)

--- test/start_suite.js
+++ test/start_suite.js
@@ -1,12 +1,31 @@
 beforeEach(function(){
 
     localStorage = {}
-    log = function(){}
+    log = function(msg){console.log(msg)}
+
+
     bot = require("../NN_start.js").bot
     expect = require("./expect.js")
 })
 
 suite("start")
+
+test("default version and prefix",function(){
+    expect(nerthus.addon.version).to.be.equal('master')
+    expect(nerthus.addon.filesPrefix).to.be.equal('http://cdn.rawgit.com/akrzyz/nerthusaddon')
+})
+
+test("fileUrl concat filesPrefix, version and file_name into url",function(){
+
+    var file = 'SCRIPT.JS'
+    var prefix = 'PREFIX'
+    var version = 'VERSION'
+    var file_url = 'PREFIX/VERSION/SCRIPT.JS'
+
+    nerthus.addon.filesPrefix = prefix
+    nerthus.addon.version = version
+    expect(nerthus.addon.fileUrl(file)).to.be.equal(file_url)
+})
 
 test("dummy", function(){
     var dummy = true

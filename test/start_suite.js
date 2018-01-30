7827684ba72a91356603f987f902f6ffa42667a3
akrzyz <akrzyz>
2018-01-30 21:45:00 +0100

    add ut for version loader

--- test/start_suite.js
+++ test/start_suite.js
@@ -3,6 +3,8 @@ before(function(){
 
     require("../NN_start.js")
     expect = require("./expect.js")
+
+    LOADED_VERSION = "LOADED_VERSION"
 })
 
 beforeEach(function(){
@@ -11,7 +13,7 @@ beforeEach(function(){
     $.loaded_scripts = []
     $.loaded_jsons = []
     $.getScript = function(url, callback){this.loaded_scripts.push(url); callback()}
-    $.getJSON = function(url, callback){this.loaded_jsons.push(url); callback({})}
+    $.getJSON = function(url, callback){this.loaded_jsons.push(url); callback({version:LOADED_VERSION})}
 
     localStorage = {}
 })
@@ -57,4 +59,15 @@ test("ScriptsLoader : load multiple scripts", function(){
     expect($.loaded_scripts).to.have.length(2)
     expect($.loaded_scripts[0]).to.be.equal(FILE_URL_1)
     expect($.loaded_scripts[1]).to.be.equal(FILE_URL_2)
+})
+
+test("VersionLoader ", function(){
+    var version = null
+    var loaded = function(v){version = v}
+
+    loader = NerthusAddonUtils.VersionLoader()
+    loader.load(loaded)
+
+    expect($.loaded_jsons).to.have.length(1)
+    expect(version).to.be.equal(LOADED_VERSION)
 })

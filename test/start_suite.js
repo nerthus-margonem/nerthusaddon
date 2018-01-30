9b458fb66ca1eef2386c3da6f57b4192c755548f
akrzyz <akrzyz>
2018-01-30 21:12:18 +0100

    add tost for loading multiple files

--- test/start_suite.js
+++ test/start_suite.js
@@ -1,11 +1,19 @@
-beforeEach(function(){
-
-    localStorage = {}
+before(function(){
     log = function(msg){console.log(msg)}
 
-
-    bot = require("../NN_start.js").bot
+    require("../NN_start.js")
     expect = require("./expect.js")
+})
+
+beforeEach(function(){
+
+    $ = {}
+    $.loaded_scripts = []
+    $.loaded_jsons = []
+    $.getScript = function(url, callback){this.loaded_scripts.push(url); callback()}
+    $.getJSON = function(url, callback){this.loaded_jsons.push(url); callback({})}
+
+    localStorage = {}
 })
 
 suite("start")
@@ -16,15 +24,37 @@ test("default version and prefix", function(){
 })
 
 test("fileUrl concat filesPrefix, version and file_name into url", function(){
+    var FILE = 'SCRIPT.JS'
+    var PREFIX = 'PREFIX'
+    var VERSION = 'VERSION'
+    var FILE_URL = 'PREFIX/VERSION/SCRIPT.JS'
 
-    var file = 'SCRIPT.JS'
-    var prefix = 'PREFIX'
-    var version = 'VERSION'
-    var file_url = 'PREFIX/VERSION/SCRIPT.JS'
-
-    nerthus.addon.filesPrefix = prefix
-    nerthus.addon.version = version
-    expect(nerthus.addon.fileUrl(file)).to.be.equal(file_url)
+    nerthus.addon.filesPrefix = PREFIX
+    nerthus.addon.version = VERSION
+    expect(nerthus.addon.fileUrl(FILE)).to.be.equal(FILE_URL)
 })
 
+test("ScriptsLoader : load single script", function(){
+    var FILE = "SCRIPT.JS"
+    var FILE_URL = nerthus.addon.fileUrl(FILE)
 
+    var loader = NerthusAddonUtils.ScriptsLoader()
+    loader.load([FILE])
+
+    expect($.loaded_scripts).to.have.length(1)
+    expect($.loaded_scripts[0]).to.be.equal(FILE_URL)
+})
+
+test("ScriptsLoader : load multiple scripts", function(){
+    var FILE_1 = "SCRIPT_1.JS"
+    var FILE_2 = "SCRIPT_2.JS"
+    var FILE_URL_1 = nerthus.addon.fileUrl(FILE_1)
+    var FILE_URL_2 = nerthus.addon.fileUrl(FILE_2)
+
+    var loader = NerthusAddonUtils.ScriptsLoader()
+    loader.load([FILE_1, FILE_2])
+
+    expect($.loaded_scripts).to.have.length(2)
+    expect($.loaded_scripts[0]).to.be.equal(FILE_URL_1)
+    expect($.loaded_scripts[1]).to.be.equal(FILE_URL_2)
+})

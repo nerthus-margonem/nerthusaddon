2297f372bc8169696c0f4a57c0867eff1a9d7636
akrzyz <akrzyz>
2018-02-05 22:55:52 +0100

    some refactor in alko

--- NN_alko.js
+++ NN_alko.js
@@ -3,117 +3,123 @@
 //==================================================================
 try
 {
-nerthus.alko = {};
+nerthus.alko = {}
 
-nerthus.alko.lvl = 0;  //stanupojenia alkoholowego 0 trzeźwy 100 urwany film.
+nerthus.alko.lvl = 0  //stanupojenia alkoholowego 0 trzeźwy 100 urwany film.
 
-nerthus.alko.timer = 0;
+nerthus.alko.timer = null
 
 nerthus.alko.run = function()
 {
-    if (getCookie('nerthus.alko') != null) 
-        this.lvl = getCookie('nerthus.alko')
-    else
-        this.lvl = 0;
+    this.lvl = 0
+    if (nerthus.addon.store() && nerthus.addon.store().nerthus_alko)
+        this.lvl = parseInt(nerthus.addon.store().nerthus_alko)
     if (this.lvl)
-        this.timer = setInterval(this.timer_handler.bind(this), 5000);
+        this.timer = setInterval(this.timer_handler.bind(this), 5000)
 }
 
 nerthus.alko.timer_handler = function()
 {
-    this.lvl--;
-    expiry = new Date(parseInt(new Date().getTime()) * 2);
-    setCookie("nerthus.alko", this.lvl, expiry)
-    if (this.lvl<1) 
+    this.lvl--
+    if(nerthus.addon.store())
+        nerthus.addon.store().nerthus_alko = this.lvl
+    if (this.lvl < 1)
     {
-        this.lvl=0;
-        clearInterval(this.timer);
-        this.timer=0
-    };
+        this.lvl = 0
+        clearInterval(this.timer)
+        this.timer = null
+    }
 }
 
+nerthus.alko.shuffleArray = function(array, cc)
+{
+    var przestanek = 0
+    if(typeof cc == 'undefined' ) cc = 0
+    if([".",",","?","!"].lastIndexOf(array[array.length-1]) > -1) przestanek = 1
+    for (var i = array.length - 1 - cc - przestanek; i > (0 + cc); i--)
+    {
+        var j = Math.floor(Math.random() * (i + 1 - cc) + cc)
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
+nerthus.alko.shuffleMessage = function(msg)
+{
+
+    if(["*/@"].includes(msg[0]) || (this.lvl <= 0))
+        return msg
+
+    switch (Math.floor(this.lvl/10))
+    {
+        case 9: msg = "/me bełkota coś niezrozumiale."
+            break
+        case 8:
+            msg = shuffleArray(msg.split(" ")).join(" ")
+        case 7:
+            t = msg.split(", ")
+            for (tt in t) t[tt]=shuffleArray(t[tt].split(" ")).join(" ")
+            msg = t.join(", ")
+        case 6:
+            t = msg.split(", ")
+            for (tt in t) t[tt]=shuffleArray(t[tt].split(" "),1).join(" ")
+            msg = t.join(", ");
+        case 5:
+            t = msg.split(" ")
+            for (tt in t) if (t[tt].length > 4) t[tt]=shuffleArray(t[tt].split("")).join("")
+            msg = t.join(" ")
+        case 4:
+            t = msg.split(" ")
+            for (tt in t) if (t[tt].length > 5) t[tt]=shuffleArray(t[tt].split("")).join("")
+            msg = t.join(" ")
+        case 3:
+            t = msg.split(" ")
+            for (tt in t) if (t[tt].length > 4) t[tt]=shuffleArray(t[tt].split(""),1).join("")
+            msg = t.join(" ")
+        case 2:
+            t = msg.split(" ");
+            for (tt in t) if (t[tt].length > 5) t[tt]=shuffleArray(t[tt].split(""),1).join("")
+            msg = t.join(" ");
+        case 1:
+            msg = msg.replace(/\.|\,|\:|\?|\!|\-/g," *hik*")
+        case 0:
+            msg = msg.replace(/\.|\,|\:|\?|\!|\-/g,"")
+    }
+    return msg
+}
+
+nerthus.alko.drink = function(c,d)
+{
+    // jeżli użyjemy towaru konsumpcyjnego o wymaganiach levelowych 18 to dodaje nam 10% upojenia alkoholowego
+    if (c.search("moveitem&st=1&id=") > -1)
+    {
+        var it = g.item[c.slice(17)]
+        if (it.cl == 16 || it.cl == 23)
+        if (it.stat.search("lvl=") > -1)
+        if (parseInt(it.stat.match(/lvl=([0-9]+)/)[1]) == 18)
+        {
+            this.lvl += 10
+            if (this.lvl > 100)
+                this.lvl = 100
+            if (!this.timer)
+                this.timer = setInterval(this.timer_handler.bind(this), 5000)
+        }
+    }
+}
 
 nerthus.alko.start = function()
 {
-    // przechwytujemy _g
-    var _nerthg = _g;
-    _g = function (c,d)
-    {
-        // jeżli użyjemy towaru konsumpcyjnego o wymaganiach levelowych 18 to dodaje nam 10% upojenia alkoholowego
-        if (c.search("moveitem&st=1&id=") > -1) 
-        {
-            var it = g.item[c.slice(17)];
-            if (it.cl == 16 || it.cl == 23)
-            if (it.stat.search("lvl=")>-1)
-            if (parseInt(it.stat.match(/lvl=([0-9]+)/)[1]) == 18)
-            {
-                this.lvl+= 10;
-                if (this.lvl > 100)
-                    this.lvl = 100
-                if (!this.timer)
-                    this.timer = setInterval(this.timer_handler.bind(this),5000);
-            }
-        }
-        _nerthg(c,d);
+    var _nerthg = _g
+    _g = function (c,d) {
+        this.drink(c,d)
+        _nerthg(c,d)
     }.bind(this)
 
-    var shuffleArray = function (array,cc) 
-    { // funkcja z netu tasujÄ?ca  tablicÄ? pozostawiajÄ?c 
-        var przestanek = 0;
-        if (typeof cc == 'undefined' ) cc = 0;
-        if ([".",",","?","!"].lastIndexOf(array[array.length-1]) > -1) przestanek = 1;
-        for (var i = array.length - 1 - cc - przestanek; i > (0+cc); i--) 
-        {
-            var j = Math.floor(Math.random() * (i + 1 - cc) + cc);
-            var temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-        }
-        return array;
-    }
-
-    var nerth_chatSendMsg = chatSendMsg;
-    chatSendMsg = function (a)
-    {
-        if ((a[0]!="*") && (a[0]!="/")&& (a[0]!="@") && (this.lvl>0))
-        {
-            switch (Math.floor(this.lvl/10))
-            {
-            case 9: a = "/me bełkota coś niezrozumiale.";
-                break;
-            case 8:
-                a = shuffleArray(a.split(" ")).join(" ");
-            case 7: 
-                t = a.split(", ");
-                for (tt in t) t[tt]=shuffleArray(t[tt].split(" ")).join(" ");
-                a = t.join(", ");
-            case 6: 
-                t = a.split(", ");
-                for (tt in t) t[tt]=shuffleArray(t[tt].split(" "),1).join(" ");
-                a = t.join(", ");
-            case 5: 
-                t = a.split(" ");
-                for (tt in t) if (t[tt].length > 4) t[tt]=shuffleArray(t[tt].split("")).join("");
-                a = t.join(" ");
-            case 4: 
-                t = a.split(" ");
-                for (tt in t) if (t[tt].length > 5) t[tt]=shuffleArray(t[tt].split("")).join("");
-                a = t.join(" ");
-            case 3: 
-                t = a.split(" ");
-                for (tt in t) if (t[tt].length > 4) t[tt]=shuffleArray(t[tt].split(""),1).join("");
-                a = t.join(" ");
-            case 2: 
-                t = a.split(" ");
-                for (tt in t) if (t[tt].length > 5) t[tt]=shuffleArray(t[tt].split(""),1).join("");
-                a = t.join(" ");
-            case 1:
-                a = a.replace(/\.|\,|\:|\?|\!|\-/g," *hik*");
-            case 0:
-                a = a.replace(/\.|\,|\:|\?|\!|\-/g,"");
-            }  
-        }    
-        nerth_chatSendMsg(a)
+    var _chatSendMsg = chatSendMsg;
+    chatSendMsg = function(msg) {
+        _chatSendMsg(this.shuffleMessage(msg))
     }.bind(this)
 
     nerthus.defer(this.run.bind(this))

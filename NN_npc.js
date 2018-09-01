83bc55c0706af2cf279075429b6cd2683cc82def
akrzyz <akrzyz>
2018-09-01 16:27:54 +0200

    huge this replacment due to not working load from storage ;(

--- NN_npc.js
+++ NN_npc.js
@@ -2,84 +2,81 @@ try
 {
 
 nerthus.npc = {}
-nerthus.npc.dialog = function()
+nerthus.npc.dialog = {}
+nerthus.npc.dialog.decorator = {}
+nerthus.npc.dialog.decorator.classes = {}
+nerthus.npc.dialog.decorator.classes.LINE = "icon LINE_OPTION"
+nerthus.npc.dialog.decorator.classes.EXIT = "icon LINE_EXIT"
+
+nerthus.npc.dialog.parse = {}
+nerthus.npc.dialog.parse.message = function(npc, index)
 {
-    var dialog = {}
-    dialog.decorator = {}
-    dialog.decorator.classes = {}
-    dialog.decorator.classes.LINE = "icon LINE_OPTION"
-    dialog.decorator.classes.EXIT = "icon LINE_EXIT"
+    return npc.dialog[index][0]
+}
+nerthus.npc.dialog.parse.replies = function(npc, index)
+{
+    var replies = []
+    for(var i = 1; i < npc.dialog[index].length; i++)
+        replies.push(nerthus.npc.dialog.parse.reply(npc.dialog[index][i], npc))
+    return replies
+}
+nerthus.npc.dialog.parse.reply = function(row_reply, npc)
+{
+    var reply = nerthus.npc.dialog.parse.row_reply(row_reply)
+    if(reply.to == "END")
+    {
+        reply.click = nerthus.npc.dialog.close.bind(nerthus.npc.dialog)
+        reply.icon = nerthus.npc.dialog.decorator.classes.EXIT
+    }
+    else if(reply.to)
+    {
+        reply.click = nerthus.npc.dialog.open.bind(nerthus.npc.dialog, npc, parseInt(reply.to))
+        reply.icon = nerthus.npc.dialog.decorator.classes.LINE
+    }
+    return reply
+}
+nerthus.npc.dialog.parse.row_reply = function(reply)
+{
+    var reply = reply.split('->')
+    return {text : reply[0], to : reply[1]}
 
-    dialog.parse = {}
-    dialog.parse.message = function(npc, index)
-    {
-        return npc.dialog[index][0]
-    }.bind(dialog)
-    dialog.parse.replies = function(npc, index)
-    {
-        var replies = []
-        for(var i = 1; i < npc.dialog[index].length; i++)
-            replies.push(this.parse.reply(npc.dialog[index][i], npc))
-        return replies
-    }.bind(dialog)
-    dialog.parse.reply = function(row_reply, npc)
-    {
-        var reply = this.parse.row_reply(row_reply)
-        if(reply.to == "END")
-        {
-            reply.click = this.close.bind(this)
-            reply.icon = this.decorator.classes.EXIT
-        }
-        else if(reply.to)
-        {
-            reply.click = this.open.bind(this, npc, parseInt(reply.to))
-            reply.icon = this.decorator.classes.LINE
-        }
-        return reply
-    }.bind(dialog)
-    dialog.parse.row_reply = function(reply)
-    {
-        var reply = reply.split('->')
-        return {text : reply[0], to : reply[1]}
+}
 
-    }.bind(dialog)
+nerthus.npc.dialog.open = function(npc, index)
+{
+    var message = nerthus.npc.dialog.parse.message(npc, index)
+    var replies = nerthus.npc.dialog.parse.replies(npc, index)
+    this.display(message, replies, npc)
+}
+nerthus.npc.dialog.display = function(message, replies, npc)
+{
+    $("#dlgin .message").empty().append(nerthus.npc.dialog.compose.message(message, npc))
+    var $replies = $("#dlgin .replies").empty()
+    $replies.append.apply($replies, replies.map(nerthus.npc.dialog.compose.reply))
+    $("#dialog").show()
+}
+nerthus.npc.dialog.close = function()
+{
+    $("#dialog").hide()
+}
 
-    dialog.open = function(npc, index)
-    {
-        var message = this.parse.message(npc, index)
-        var replies = this.parse.replies(npc, index)
-        this.display(message, replies, npc)
-    }
-    dialog.display = function(message, replies, npc)
-    {
-        $("#dlgin .message").empty().append(this.compose.message(message, npc))
-        var $replies = $("#dlgin .replies").empty()
-        $replies.append.apply($replies, replies.map(this.compose.reply))
-        $("#dialog").show()
-    }
-    dialog.close = function()
-    {
-        $("#dialog").hide()
-    }
-
-    dialog.compose = {}
-    dialog.compose.icon = function(type)
-    {
-        return $("<div>").addClass(type)
-    }
-    dialog.compose.reply = function(reply)
-    {
-        return $("<li>").addClass(reply.icon)
-                        .append(this.icon(reply.icon))
-                        .append(reply.text)
-                        .click(reply.click)
-    }.bind(dialog.compose)
-    dialog.compose.message = function(message, npc)
-    {
-        return "<h4><b>" + npc.name + "</b></h4>" + message
-    }
-    return dialog
-}()
+nerthus.npc.dialog.compose = {}
+nerthus.npc.dialog.compose.icon = function(type)
+{
+    return $("<div>").addClass(type)
+}
+nerthus.npc.dialog.compose.reply = function(reply)
+{
+    var icon = nerthus.npc.dialog.compose.icon(reply.icon)
+    return $("<li>").addClass(reply.icon)
+                    .append(icon)
+                    .append(reply.text)
+                    .click(reply.click)
+}
+nerthus.npc.dialog.compose.message = function(message, npc)
+{
+    return "<h4><b>" + npc.name + "</b></h4>" + message
+}
 
 nerthus.npc.deploy = function(npc)
 {

f19da7cae706ba4fe14d78d95dff1a6a48a82c25
akrzyz <akrzyz>
2018-09-01 14:39:24 +0200

    some rework with dialog

--- NN_npc.js
+++ NN_npc.js
@@ -4,72 +4,83 @@ try
 nerthus.npc = {}
 nerthus.npc.dialog = function()
 {
-    var UNPACK_QUERY = function(query)
-    {
-        return query[0].outerHTML
-    }
     var dialog = {}
     dialog.decorator = {}
     dialog.decorator.classes = {}
-    dialog.decorator.classes.LINE = "LINE_OPTION"
-    dialog.decorator.classes.EXIT = "LINE_EXIT"
-    dialog.decorator.classes.ICON = "icon"
-    dialog.decorator.icon = function(type)
+    dialog.decorator.classes.LINE = "icon LINE_OPTION"
+    dialog.decorator.classes.EXIT = "icon LINE_EXIT"
+
+    dialog.parse = {}
+    dialog.parse.message = function(npc, index)
     {
-        return $("<div>").addClass(type)
-    }
-    dialog.decorator.header = function(text)
-    {
-        return $("<h4>").html(text)[0].outerHTML
-    }
-    dialog.decorator.point = function(text)
-    {
-        return $("<li>").html(text)
-    }
-    dialog.open = function(npc, index)
-    {
-        var message = this.message(npc, index)
-        var replies = this.replies(npc, index)
-        this.display(message, replies)
-    }
-    dialog.message = function(npc, index)
-    {
-        return this.decorator.header(npc.name) + npc.dialog[index][0]
-    }
-    dialog.replies = function(npc, index)
+        return npc.dialog[index][0]
+    }.bind(dialog)
+    dialog.parse.replies = function(npc, index)
     {
         var replies = []
         for(var i = 1; i < npc.dialog[index].length; i++)
-            replies.push(UNPACK_QUERY(this.reply(npc.dialog[index][i])))
+            replies.push(this.parse.reply(npc.dialog[index][i], npc))
         return replies
-    }
-    dialog.reply = function(reply)
+    }.bind(dialog)
+    dialog.parse.reply = function(row_reply, npc)
     {
-        var parsed = this.parse_reply(reply)
-        var $replay = this.decorator.point(parsed[0])
-        if(parsed.length === 2)
+        var reply = this.parse.row_reply(row_reply)
+        if(reply.to)
         {
-            $replay.addClass(this.decorator.classes.ICON)
-            if(parsed[1] == "END")
-                $replay.addClass(this.decorator.classes.EXIT)
+            if(reply.to == "END")
+            {
+                reply.click = this.close.bind(this)
+                reply.icon = this.decorator.classes.EXIT
+            }
             else
-                $replay.addClass(this.decorator.classes.LINE)
+            {
+                reply.click = this.open.bind(this, npc, parseInt(reply.to))
+                reply.icon = this.decorator.classes.LINE
+            }
         }
-        return $replay
-    }
-    dialog.parse_reply = function(replay)
+        return reply
+    }.bind(dialog)
+    dialog.parse.row_reply = function(reply)
     {
-        return replay.split('->')
-    }
-    dialog.display = function(message, replies)
+        var reply = reply.split('->')
+        return {text : reply[0], to : reply[1]}
+
+    }.bind(dialog)
+
+
+    dialog.open = function(npc, index)
     {
-        $("#dialog message").html(message)
-        $("#dialog replies").html(replies)
+        var message = this.parse.message(npc, index)
+        var replies = this.parse.replies(npc, index)
+        this.display(message, replies, npc)
+    }
+    dialog.display = function(message, replies, npc)
+    {
+        $("#dlgin .message").empty().append(this.compose.message(message, npc))
+        var $replies = $("#dlgin .replies").empty()
+        $replies.append.apply($replies, replies.map(this.compose.reply))
         $("#dialog").show()
     }
-    dialog.hide = function()
+    dialog.close = function()
     {
         $("#dialog").hide()
+    }
+
+    dialog.compose = {}
+    dialog.compose.icon = function(type)
+    {
+        return $("<div>").addClass(type)
+    }
+    dialog.compose.reply = function(reply)
+    {
+        return $("<li>").addClass(reply.icon)
+                        .append(this.icon(reply.icon))
+                        .append(reply.text)
+                        .click(reply.click)
+    }.bind(dialog.compose)
+    dialog.compose.message = function(message, npc)
+    {
+        return "<h4><b>" + npc.name + "</b></h4>" + message
     }
     return dialog
 }()
--- test/npc_suite.js
+++ test/npc_suite.js
@@ -1,4 +1,3 @@
-suite("npc")
 
 before(function()
 {
@@ -21,88 +20,139 @@ before(function()
     NPC.y = 4
     NPC.dialog =
     {
-        0 :
+        0 : //Simple dialog
         [
             "Hej wam!",
             "A no Hej",
             "Elo"
         ],
-        1 :
+        1 : //normal dialog
         [
             "A jakoś leci",
             "a tak sobie... ->2",
             "dobrze ->END"
+        ],
+        2 : //normal dialog
+        [
+            "To już koniec",
+            "a no konie ->END"
         ]
     }
 
     dialog = nerthus.npc.dialog
 
+    $dialog = $("<div>").attr("id","dialog")
+    .append($("<div>").attr("id","dlgin")
+            .append($("<div>").attr("id","message").addClass("message"))
+            .append($("<div>").attr("id","replies").addClass("replies")))
+    .appendTo("body")
+
 })
 
-test("message shall includes npc name and first dialog from given index", function()
+suite("npc dialog parse message")
+
+test("message shall includes first dialog from given index", function()
 {
     const INDEX = 0
-    const message = dialog.message(NPC,INDEX)
-    expect(message).to.contain(NPC.name)
+    const message = dialog.parse.message(NPC,INDEX)
     expect(message).to.contain(NPC.dialog[INDEX][0])
 })
 
-test("npc name in message shall be decorated by <h4>", function()
-{
-    const INDEX = 0
-    const message = dialog.message(NPC,INDEX)
-    expect(message).to.contain("<h4>" + NPC.name + "</h4>")
-})
+suite("npc dialog parse replies")
 
 test("replies shall includes dialogs from given index expect first one, first is message", function()
 {
     const INDEX = 0
-    const replies = dialog.replies(NPC,INDEX)
-    expect(replies).to.have.length(2)
-    expect(replies[0]).to.contain(NPC.dialog[INDEX][1])
-    expect(replies[1]).to.contain(NPC.dialog[INDEX][2])
-})
+    const replies = dialog.parse.replies(NPC,INDEX)
 
-test("replies shall be decorated by <li>", function()
-{
-    const INDEX = 0
-    const replies = dialog.replies(NPC,INDEX)
-    const is_decorated = /^<li>.*<\/li>/
-    expect(replies[0]).to.match(is_decorated)
-    expect(replies[1]).to.match(is_decorated)
+    expect(replies).to.have.length(2)
+
+    expect(replies[0].text).to.contain(NPC.dialog[INDEX][1])
+    expect(replies[0].click).to.not.ok()
+    expect(replies[0].icon).to.not.ok()
+
+    expect(replies[1].text).to.contain(NPC.dialog[INDEX][2])
+    expect(replies[1].click).to.not.ok()
+    expect(replies[1].icon).to.not.ok()
 })
 
 test("replies shall not contains arrows (->)", function()
 {
     const INDEX = 1
-    const replies = dialog.replies(NPC,INDEX)
+    const replies = dialog.parse.replies(NPC,INDEX)
     const ARROW = "->"
 
     expect(NPC.dialog[INDEX][1]).to.contain(ARROW)
     expect(NPC.dialog[INDEX][2]).to.contain(ARROW)
+
     expect(replies).to.have.length(2)
-    expect(replies[0]).to.not.contain(ARROW)
-    expect(replies[1]).to.not.contain(ARROW)
+    expect(replies[0].text).to.not.contain(ARROW)
+    expect(replies[1].text).to.not.contain(ARROW)
 })
 
-test("replies with ->END shall has LINE_EXIT class", function()
+test("replies with ->END shall has LINE_EXIT class and click handler", function()
 {
     const INDEX = 1
-    const replies = dialog.replies(NPC,INDEX)
+    const replies = dialog.parse.replies(NPC,INDEX)
     const END_MARKER = "->END"
 
     expect(NPC.dialog[INDEX][2]).to.contain(END_MARKER)
-    expect( $(replies[1]).hasClass(dialog.decorator.classes.EXIT) ).ok()
-    expect( $(replies[1]).hasClass(dialog.decorator.classes.ICON) ).ok()
+
+    expect(replies[1].icon).to.be(dialog.decorator.classes.EXIT)
+    expect(replies[1].click).to.be.ok()
 })
 
-test("replies with ->$LINE shall has LINE_OPTION class", function()
+test("replies with ->$LINE shall has LINE_OPTION class and click handler", function()
 {
     const INDEX = 1
-    const replies = dialog.replies(NPC,INDEX)
+    const replies = dialog.parse.replies(NPC,INDEX)
     const GO_TO_LINE_MARKER = /->\d+/
 
     expect(NPC.dialog[INDEX][1]).to.match(GO_TO_LINE_MARKER)
-    expect( $(replies[0]).hasClass(dialog.decorator.classes.LINE) ).ok()
-    expect( $(replies[0]).hasClass(dialog.decorator.classes.ICON) ).ok()
+
+    expect(replies[0].icon).to.be(dialog.decorator.classes.LINE)
+    expect(replies[0].click).to.be.ok()
+})
+
+suite("npc dialog API")
+
+test("open dialog display dialog from given index", function()
+{
+    const INDEX = 0
+    dialog.open(NPC,INDEX)
+
+    expect($("#dialog #dlgin .message").html()).to.contain(NPC.name)
+    expect($("#dialog #dlgin .message").html()).to.contain(NPC.dialog[INDEX][0])
+
+    expect($("#dialog #dlgin .replies").html()).to.contain(NPC.dialog[INDEX][1])
+    expect($("#dialog #dlgin .replies").html()).to.contain(NPC.dialog[INDEX][2])
+})
+
+test("reply with ->$LINE go to next dialog", function()
+{
+    const BEGIN_INDEX = 1
+    const NEXT_INDEX = 2
+    dialog.open(NPC,BEGIN_INDEX)
+
+    expect($("#dialog #dlgin .message").html()).to.contain(NPC.name)
+    expect($("#dialog #dlgin .message").html()).to.contain(NPC.dialog[BEGIN_INDEX][0])
+
+    $("#dialog #dlgin .replies li")[0].click() // dialog[1][1] go to dialog 2
+    expect($("#dialog #dlgin .message").html()).to.contain(NPC.name)
+    expect($("#dialog #dlgin .message").html()).to.contain(NPC.dialog[NEXT_INDEX][0])
+})
+
+test("reply with ->END close dialog", function()
+{
+    const INDEX = 1
+    dialog.open(NPC,INDEX)
+
+    expect($("#dialog #dlgin .message").html()).to.contain(NPC.name)
+    expect($("#dialog #dlgin .message").html()).to.contain(NPC.dialog[INDEX][0])
+
+    expect($("#dialog").css("display")).to.be("block")
+
+    $("#dialog #dlgin .replies li")[1].click() //dialog[1][2] go to END
+
+    expect($("#dialog").css("display")).to.be("none")
 })

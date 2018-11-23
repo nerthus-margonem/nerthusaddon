e8da68626179fc0e4bb44e65d0d702584339f06e
akrzyz <aleksander.krzyz@gmail.com>
2018-11-23 22:43:13 +0100

    fix light mgr - coma after dumping last element removed

--- NN_night_lights_mgr.js
+++ NN_night_lights_mgr.js
@@ -7,12 +7,12 @@ nerthus.night.lights.make_testable = function(light)
     .css({pointerEvents : "auto",
           border : "1px solid yellow"})
     .dblclick(function(){light.remove();})
-    .draggable();
+    .draggable()
 }
 
 nerthus.night.lights.add_testable = function(light)
 {
-    this.make_testable(this.display(light));          
+    this.make_testable(this.display(light))
 }
 
 nerthus.night.lights.dump = function()
@@ -21,28 +21,30 @@ nerthus.night.lights.dump = function()
     log("[")
     this.log_lights()
     log("]")
-    message("dumping done");
+    message("dumping done")
 }
 
 nerthus.night.lights.log_lights = function()
 {
+    var lights = []
     $("#base .nightLight").each(function()
     {
             var pos = $(this).position()
             var x = parseInt(pos.left)
             var y = parseInt(pos.top)
             var type = $(this).attr("type")
-            log('{"x" : "' +x+ '", "y" : "' +y+ '", "type" : "' +type+ '"},')
+            lights.push('{"x" : "' +x+ '", "y" : "' +y+ '", "type" : "' +type+ '"}')
     });
+    log(lights.join(",<br>"))
 }
 
 nerthus.night.lights.give_me_the_light = function()
 {
-    $("#base .nightLight").css({pointerEvents : "auto"}).each(function(){nerthus.night.lights.make_testable($(this))});
-    var dumpLight = $("<span>dump lights</span>").click(function(){nerthus.night.lights.dump()});
-    var addBorder = $("<span>add border</span>").click(function(){$("#base .nightLight").css("border","1px solid yellow")});
-    var delBorder = $("<span>del border</span>").click(function(){$("#base .nightLight").css("border","")});
-    var togglemouseMove = $("<span>toggle move</span>").click(function(){hero.opt ^= 64; message("blocked: " + Boolean(hero.opt & 64))});
+    $("#base .nightLight").css({pointerEvents : "auto"}).each(function(){nerthus.night.lights.make_testable($(this))})
+    var dumpLight = $("<span>dump lights</span>").click(function(){nerthus.night.lights.dump()})
+    var addBorder = $("<span>add border</span>").click(function(){$("#base .nightLight").css("border","1px solid yellow")})
+    var delBorder = $("<span>del border</span>").click(function(){$("#base .nightLight").css("border","")})
+    var togglemouseMove = $("<span>toggle move</span>").click(function(){hero.opt ^= 64; message("blocked: " + Boolean(hero.opt & 64))})
     $.getScript("http://addons2.margonem.pl/get/1/1689public.js",function()
     {
         for(i in nerthus.night.lights.types)
@@ -51,11 +53,11 @@ nerthus.night.lights.give_me_the_light = function()
                 {
                     var light = {'type' : $(this).attr("type"), 'x' : hero.x*32, 'y' : hero.y*32}
                     nerthus.night.lights.add_testable(light)
-                }));                 
-        aldiMenu.add(dumpLight);
-        aldiMenu.add(addBorder);
-        aldiMenu.add(delBorder);
-        aldiMenu.add(togglemouseMove);
+                }))
+        aldiMenu.add(dumpLight)
+        aldiMenu.add(addBorder)
+        aldiMenu.add(delBorder)
+        aldiMenu.add(togglemouseMove)
     });
 }
 

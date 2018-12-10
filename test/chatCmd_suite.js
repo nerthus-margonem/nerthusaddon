8da18f8affde4995ea776a9a269d19bb85b0b876
KrisAphalon <Krzysztof.Kacz@interia.pl>
2018-12-10 05:12:42 +0100

    Added test to fixing links

--- test/chatCmd_suite.js
+++ test/chatCmd_suite.js
@@ -24,3 +24,8 @@ test("dummy", function()
 {
 })
 
+test("zmiana https*Krzywi się.*/ na poprawny link", function() {
+    let buggedText = "https*Krzywi się.*/www.link.pl/images/image.gif",
+        fixedText = "https://www.link.pl/images/image.gif"
+    expect(nerthus.chatCmd.extractUrlFromDecorator(buggedText)).to.be(fixedText)
+})

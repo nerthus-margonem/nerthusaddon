e16bd2d906ff14a8ccf9d3707ebd7f20614f8b66
KrisAphalon <Krzysztof.Kacz@interia.pl>
2019-08-17 17:34:31 +0200

    added translate tests

--- test/panel_suite.js
+++ test/panel_suite.js
@@ -5,10 +5,24 @@ before(function()
     nerthus = {}
     nerthus.defer = function(){}
 
+
+
     expect = require("expect.js")
     require("../NN_panel.js")
+
+    nerthus.panel.settings_translations = {
+        "translate": "przetłumaczony"
+    }
 })
 
 test("dummy", function()
 {
+})
+
+test("translate",() =>{
+    expect(nerthus.panel.translate_option("translate")).to.equal("przetłumaczony")
+})
+
+test("translate untranslatable",() =>{
+    expect(nerthus.panel.translate_option("untranslatable")).to.equal("untranslatable")
 })

4f67167c379aae6280702ca600246f85ee64c3df
KrisAphalon <Krzysztof.Kacz@interia.pl>
2020-03-20 11:37:14 +0100

    Fully converted tips

--- src/permissions.js
+++ src/permissions.js
@@ -6,7 +6,7 @@
     3: xxxx
     100: dev or other spec
  */
-
+export const PERMISSION_LVL = {DEFAULT: 0, NARRATOR: 1, COUNCILOR: 2, SPEC: 100}
 export function checkPermissionLvl(nick) {
     return 2
 }

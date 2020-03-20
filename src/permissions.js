d314403405f028a888890eaf45ed3215794fbf06
KrisAphalon <Krzysztof.Kacz@interia.pl>
2020-03-20 21:40:46 +0100

    Added permissions

--- src/permissions.js
+++ src/permissions.js
@@ -1,12 +1,21 @@
-
+import {default as permissions} from './res/permissions'
 /*
     0: normal player
     1: narrator
-    2: radny
-    3: xxxx
-    100: dev or other spec
+    2: councilor (radny)
+    100: dev or other special reason
  */
-export const PERMISSION_LVL = {DEFAULT: 0, NARRATOR: 1, COUNCILOR: 2, SPEC: 100}
-export function checkPermissionLvl(nick) {
-    return 2
+export const PERMISSION_LVL = {DEFAULT: 0, NARRATOR: 1, COUNCILOR: 2, SPECIAL: 100}
+
+export function checkPermissionLvl(nick)
+{
+    if (permissions.radny.indexOf(nick) >= 0) return PERMISSION_LVL.COUNCILOR
+    if (permissions.narrator.indexOf(nick) >= 0) return PERMISSION_LVL.NARRATOR
+    if (permissions.special.indexOf(nick) >= 0) return PERMISSION_LVL.SPECIAL
+    return PERMISSION_LVL.DEFAULT
+}
+
+export function hasNarrationRights(nick)
+{
+    return checkPermissionLvl(nick) > PERMISSION_LVL.DEFAULT
 }

fc53a09b88eb32b27db6cd50fd577d9633d5ae1b
KrisAphalon <Krzysztof.Kacz@interia.pl>
2020-03-24 15:36:27 +0100

    Implemented alcohol drinking

---
+++ src/chat/chat-drunkenness.js
@@ -0,0 +1,165 @@
+let intoxicationLvl = 0
+let decayTimerID = null
+
+function decayTimerHandler()
+{
+    intoxicationLvl--
+    localStorage.setItem('nerthus-intoxication-lvl', intoxicationLvl)
+
+    if (intoxicationLvl < 1)
+    {
+        intoxicationLvl = 0
+        clearInterval(decayTimerID)
+        decayTimerID = null
+    }
+}
+
+function shuffleArray(array, cc) // sic
+{
+    var przestanek = 0
+    if (typeof cc == 'undefined') cc = 0
+    if (['.', ',', '?', '!'].lastIndexOf(array[array.length - 1]) > -1) przestanek = 1
+    for (var i = array.length - 1 - cc - przestanek; i > (0 + cc); i--)
+    {
+        var j = Math.floor(Math.random() * (i + 1 - cc) + cc)
+        var temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+    }
+    return array
+}
+
+function shuffleMessage(msg)
+{
+    if (intoxicationLvl === 0 || ['*/@'].indexOf(msg[0]) >= 0)
+        return msg
+
+    let t = []
+    switch (Math.floor(this.lvl / 10))
+    {
+        case 10:
+        case 9:
+            msg = '/me bełkota coś niezrozumiale.'
+            break
+        case 8:
+            msg = shuffleArray(msg.split(' ')).join(' ')
+        // fallthrough
+        case 7:
+            t = msg.split(', ')
+            for (const index in t)
+                t[index] = shuffleArray(t[index].split(' ')).join(' ')
+            msg = t.join(', ')
+        // fallthrough
+        case 6:
+            t = msg.split(', ')
+            for (const index in t)
+                t[index] = shuffleArray(t[index].split(' '), 1).join(' ')
+            msg = t.join(', ')
+        // fallthrough
+        case 5:
+            t = msg.split(' ')
+            for (const index in t)
+                if (t[index].length > 4)
+                    t[index] = shuffleArray(t[index].split('')).join('')
+            msg = t.join(' ')
+        // fallthrough
+        case 4:
+            t = msg.split(' ')
+            for (const index in t)
+                if (t[index].length > 5)
+                    t[index] = shuffleArray(t[index].split('')).join('')
+            msg = t.join(' ')
+        // fallthrough
+        case 3:
+            t = msg.split(' ')
+            for (const index in t)
+                if (t[index].length > 4)
+                    t[index] = shuffleArray(t[index].split(''), 1).join('')
+            msg = t.join(' ')
+        // fallthrough
+        case 2:
+            t = msg.split(' ')
+            for (const index in t)
+                if (t[index].length > 5)
+                    t[index] = shuffleArray(t[index].split(''), 1).join('')
+            msg = t.join(' ')
+        // fallthrough
+        case 1:
+            msg = msg.replace(/[.,:?!\-]/g, ' *hik*')
+            break
+        case 0:
+            msg = msg.replace(/[.,:?!\-]/g, '')
+    }
+    return msg
+}
+
+function commandContainsDrinkingAlcohol(command)
+{
+    const match = command.match(/^moveitem.*id=(\d+)/)
+    if (match)
+    {
+        let item
+        if (INTERFACE === 'NI')
+        {
+            item = Engine.items.getItemById(match[1])
+        }
+        else
+        {
+            item = g.item[match[1]]
+        }
+
+        if (item && (item.cl === 16) && // cl = 16 means it's consumable
+            item.stat.search('lvl=') > -1 &&
+            parseInt(item.stat.match(/lvl=([0-9]+)/)[1]) === 18
+        )
+            return true
+    }
+    return false
+}
+
+export function initChatDrunkenness()
+{
+    const savedIntoxicationLvl = localStorage.getItem('nerthus-intoxication-lvl')
+    if (savedIntoxicationLvl)
+        intoxicationLvl = parseInt(savedIntoxicationLvl)
+    if (intoxicationLvl)
+        decayTimerID = setInterval(decayTimerHandler, 10000)
+
+    const __g = _g
+    window._g = function (task, callback, payload)
+    {
+        if (commandContainsDrinkingAlcohol(task))
+        {
+            intoxicationLvl += 10
+            if (intoxicationLvl > 100) intoxicationLvl = 100
+
+            if (!decayTimerID)
+                decayTimerID = setInterval(decayTimerHandler, 10000)
+        }
+        __g(task, callback, payload)
+    }
+
+    if (INTERFACE === 'NI')
+    {
+        const _chatSendMsg = Engine.chat.sendMessage
+        Engine.chat.sendMessage = function (msg)
+        {
+            _chatSendMsg.call(Engine.chat, shuffleMessage(msg))
+        }
+    }
+    else
+    {
+        const _chatSendMsg = window.chatSendMsg
+        window.chatSendMsg = function (msg)
+        {
+            _chatSendMsg(shuffleMessage(msg))
+        }
+    }
+}
+
+/**
+ * Original version license:
+ * ==================================================================
+ * OBSŁUGA PICIA - Autor Godfryd
+ * ==================================================================
+ */

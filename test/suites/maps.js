1c0cb53ff3f093edf811c4beecc15d84fd70f7ed
KrisAphalon <Krzysztof.Kacz@interia.pl>
2020-04-06 17:07:54 +0200

    Added more tests to maps suite

--- test/suites/maps.js
+++ test/suites/maps.js
@@ -9,7 +9,8 @@ export function testMaps()
 
         const loadOnEveryMap = sinon.fake()
         const resolveNerthusUrl = sinon.fake.returns('resolved-url')
-        const getCurrentSeason = sinon.fake.returns('curent-season')
+        const getCurrentSeason = sinon.fake.returns('current-season')
+        const inject = require('inject-loader!babel-loader!../../src/maps')
 
         describe('applyCurrentMapChange()', function ()
         {
@@ -21,30 +22,113 @@ export function testMaps()
                 $mapImage = $('<div id="ground"></div>').appendTo('body')
             })
             afterEach(resetWindow)
-            it('should not change anything when no map', function ()
+            it('should not change anything when no custom map defined', function ()
             {
-                const maps = require('../../src/maps')
-                const image = $mapImage.css('background-image')
-                maps.applyCurrentMapChange()
-                expect($mapImage.css('background-image')).to.be.equal(image)
-            })
-
-            it('should change map when season map is defined', function ()
-            {
-                const inject = require('inject-loader!babel-loader!../../src/maps')
                 const maps = inject({
                     './game-integration/loaders': {loadOnEveryMap},
                     './time': {getCurrentSeason},
                     '../res/configs/maps.json': {
                         'default': {},
-                        'curent-season': {1: 'map-url'}
+                        'current-season': {1: 'map-url'}
                     },
                     './utility-functions': {resolveNerthusUrl}
                 })
 
+                window.map.id = 0
+                const image = $mapImage.css('background-image')
+                maps.applyCurrentMapChange()
+                expect($mapImage.css('background-image')).to.be.equal(image)
+            })
+
+            it('should change map when there is custom map in current season', function ()
+            {
+                const maps = inject({
+                    './game-integration/loaders': {loadOnEveryMap},
+                    './time': {getCurrentSeason},
+                    '../res/configs/maps.json': {
+                        'default': {},
+                        'current-season': {1: 'map-url'}
+                    },
+                    './utility-functions': {resolveNerthusUrl}
+                })
+                window.map.id = 1
 
                 maps.applyCurrentMapChange()
                 expect($mapImage.css('background-image')).to.be.equal(`url(${resolveNerthusUrl()})`)
+            })
+
+            it('should change map when there is custom map in default', function ()
+            {
+                const maps = inject({
+                    './game-integration/loaders': {loadOnEveryMap},
+                    './time': {getCurrentSeason},
+                    '../res/configs/maps.json': {
+                        'default': {1: 'map-url'},
+                        'current-season': {}
+                    },
+                    './utility-functions': {resolveNerthusUrl}
+                })
+                window.map.id = 1
+
+                maps.applyCurrentMapChange()
+                expect($mapImage.css('background-image')).to.be.equal(`url(${resolveNerthusUrl()})`)
+            })
+
+            it('should load season map when both default and season maps are defined', function ()
+            {
+                const customResolveNerthusUrl = (str) => str
+                const maps = inject({
+                    './game-integration/loaders': {loadOnEveryMap},
+                    './time': {getCurrentSeason},
+                    '../res/configs/maps.json': {
+                        'default': {1: 'default-map-url'},
+                        'current-season': {1: 'season-map-url'}
+                    },
+                    './utility-functions': {resolveNerthusUrl: customResolveNerthusUrl}
+                })
+                window.map.id = 1
+
+                maps.applyCurrentMapChange()
+                expect($mapImage.css('background-image')).to.be.equal(
+                    `url(${customResolveNerthusUrl('season-map-url')})`
+                )
+            })
+
+            it('should not change map when there is custom map but not in current season', function ()
+            {
+                const maps = inject({
+                    './game-integration/loaders': {loadOnEveryMap},
+                    './time': {getCurrentSeason},
+                    '../res/configs/maps.json': {
+                        'default': {},
+                        'current-season': {},
+                        'other-season': {1: 'map-url'}
+                    },
+                    './utility-functions': {resolveNerthusUrl}
+                })
+                window.map.id = 1
+
+                const image = $mapImage.css('background-image')
+                maps.applyCurrentMapChange()
+                expect($mapImage.css('background-image')).to.be.equal(image)
+            })
+
+            it('should not change map when there is custom map but wrong id', function ()
+            {
+                const maps = inject({
+                    './game-integration/loaders': {loadOnEveryMap},
+                    './time': {getCurrentSeason},
+                    '../res/configs/maps.json': {
+                        'default': {},
+                        'current-season': {2: 'map-url'}
+                    },
+                    './utility-functions': {resolveNerthusUrl}
+                })
+                window.map.id = 1
+
+                const image = $mapImage.css('background-image')
+                maps.applyCurrentMapChange()
+                expect($mapImage.css('background-image')).to.be.equal(image)
             })
         })
     })

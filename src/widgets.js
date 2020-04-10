f645d64355c29669baeda846dbffc4983aa73135
KrisAphalon <Krzysztof.Kacz@interia.pl>
2020-04-10 03:38:40 +0200

    Remove leftover console.logs

--- src/widgets.js
+++ src/widgets.js
@@ -2,7 +2,6 @@ let $widgetsContainer
 
 export function addWidget(name, imageUrl, description) // TODO NI
 {
-    console.log('addingwidget')
     return $(
         '<div id="' + name + '-widget" class="nerthus__widget">' +
         '<div class="nerthus__widget-image" style="background-image: url(' + imageUrl + ')"></div>' +

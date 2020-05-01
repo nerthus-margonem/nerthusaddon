cf15220b5400120f6253585307a4c8bdc1acaf59
KrisAphalon <Krzysztof.Kacz@interia.pl>
2020-05-01 21:29:39 +0200

    Added better climate gradient over seasons and when switching maps

--- src/weather/effects.js
+++ src/weather/effects.js
@@ -1,3 +1,55 @@
+import {addToNiDrawList} from '../game-integration/loaders'
+import {coordsToId} from '../utility-functions'
+
+function getDarknessNiObject(opacity)
+{
+    return {
+        draw: function (e)
+        {
+            const name = nerthus.worldEdit.currentWeatherEffects[i][0]
+            const opacity = nerthus.worldEdit.currentWeatherEffects[i][1]
+            const img = nerthus.worldEdit.weatherImages[name][nerthus.worldEdit.weatherCurrentFrameNumbers[name] + 1]
+
+            //check if img has been loaded correctly to not stop entire game in case of error
+            if (img.complete && img.naturalWidth !== 0)
+            {
+                const pattern = e.createPattern(img, 'repeat')
+                const style = e.fillStyle
+                const alpha = e.globalAlpha
+                e.fillStyle = pattern
+                e.globalAlpha = opacity
+                e.translate(0 - Engine.map.offset[0], 0 - Engine.map.offset[1])
+                e.fillRect(0, 0, Engine.map.width, Engine.map.height)
+                e.translate(Engine.map.offset[0], Engine.map.offset[1])
+                e.fillStyle = style
+                e.globalAlpha = alpha
+            }
+
+            const style = e.fillStyle
+            e.fillStyle = '#000'
+            e.globalAlpha = opacity
+            e.fillRect(0 - Engine.map.offset[0], 0 - Engine.map.offset[1], Engine.map.width, Engine.map.height)
+            e.globalAlpha = 1.0
+            e.fillStyle = style
+        },
+        getOrder: function ()
+        {
+            return 940 // Darkness bellow lights but above everything else
+        },
+        update: function (e) { console.log(e)},
+        frame: 0,
+        frames: [
+            {delay: 100},
+            {delay: 100},
+            {delay: 100},
+            {delay: 100}
+        ],
+        frameAmount: 4,
+        activeFrame: 0
+    }
+}
+
+
 export function clearEffects()
 {
     if (INTERFACE === 'NI')
@@ -14,7 +66,7 @@ export function displayRain(opacity)
 {
     if (INTERFACE === 'NI')
     {
-
+        addToNiDrawList(getDarknessNiObject(opacity), coordsToId(-1, -1))
     }
     else
     {
@@ -26,6 +78,7 @@ export function displayRain(opacity)
             })
             .appendTo('#ground')
     }
+
 }
 
 export function displaySnow(opacity)

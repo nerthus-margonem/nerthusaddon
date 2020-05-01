dcf61b63323a917e398e6963f3c6724c4d896fd8
KrisAphalon <Krzysztof.Kacz@interia.pl>
2020-05-01 21:31:15 +0200

    Added isCurrentMapOutoor utility function

--- src/utility-functions.js
+++ src/utility-functions.js
@@ -1,3 +1,5 @@
+import {default as exceptionMaps} from '../res/configs/map-exceptions.json'
+
 /**
  * Function that takes coordinates and turns them to nerthus-specific ID.
  * NI requires such id in several cases, and really big number makes sure,
@@ -20,4 +22,22 @@ export function pseudoRandom(seed)
 {
     seed = (seed * 9301 + 49297) % 233280
     return seed / 233280
+}
+
+export function isMapOutdoor(map)
+{
+    return !!((map.mainid === 0 && !exceptionMaps.indoor.includes(map.id))
+        || exceptionMaps.outdoor.includes(map.id)
+    )
+}
+
+export function isCurrentMapOutdoor() {
+    if (INTERFACE === 'NI')
+    {
+        return isMapOutdoor(Engine.map.d)
+    }
+    else
+    {
+        return isMapOutdoor(map)
+    }
 }

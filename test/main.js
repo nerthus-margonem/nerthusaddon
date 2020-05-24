072ad54da295fea0c713c5064f54625ffa9ae8a6
KrisAphalon <Krzysztof.Kacz@interia.pl>
2020-05-24 02:26:19 +0200

    Added tests for resources

--- test/main.js
+++ test/main.js
@@ -1,12 +1,14 @@
 import {testMaps} from './suites/maps'
 import * as cheerio from 'cheerio'
 import {testZodiac} from './suites/zodiac'
+import {testNpcsFiles} from './suites/configs/npcs'
+import {testNightLights} from './suites/configs/night-lights'
 
 function init()
 {
     global.resetWindow = function ()
     {
-        global.$ = cheerio.load('');
+        global.$ = cheerio.load('')
         global.window = {}
         global.document = {}
     }
@@ -21,5 +23,9 @@ function init()
 }
 
 init()
+
+testNightLights()
+testNpcsFiles()
+
 testMaps()
 testZodiac()

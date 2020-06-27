ad698181ff87cd39f0bf9bf5cd2ad29a330d692d
KrisAphalon <Krzysztof.Kacz@interia.pl>
2020-06-27 02:28:07 +0200

    Added light-manager module with most functionalities working

--- src/night/light-manager.js
+++ src/night/light-manager.js
@@ -1,3 +1,5 @@
+import {lightsOn, turnLightsOn} from './lights'
+
 const $style = $('<style>')
 
 function removeLight($light)
@@ -20,6 +22,47 @@ function startEditingLigths()
     })
 }
 
+const $lightHider = $('<style>').appendTo('head')
+
+function toggleLights()
+{
+    if (!lightsOn)
+    {
+        turnLightsOn()
+        $('#nerthus-light-manager-toggle-lights').addClass('blue')
+    }
+    else if ($lightHider.text() === '')
+    {
+        $lightHider.text('#ground > .nerthus__night-light {display: none !important}')
+        $('#nerthus-light-manager-toggle-lights').removeClass('blue')
+    }
+    else
+    {
+        $lightHider.text('')
+        $('#nerthus-light-manager-toggle-lights').addClass('blue')
+    }
+}
+
+function toggleMouseMove()
+{
+    hero.opt ^= 64
+    $('#nerthus-light-manager-toggle-mousemove').toggleClass('blue')
+}
+
+const $borderStyle = $('<style>').appendTo('head')
+
+function toggleBorder()
+{
+    if ($borderStyle.text() === '')
+    {
+        $borderStyle.text('#ground > .nerthus__night-light {border: 1px solid yellow}')
+    }
+    else
+    {
+        $borderStyle.text('')
+    }
+}
+
 function stopEditingLights()
 {
     $style.text('')
@@ -39,12 +82,19 @@ function downloadLog()
         }
         arr.push(obj)
     })
-    // const a = window.document.createElement('a')
-    // a.href = window.URL.createObjectURL(new window.Blob([JSON.stringify(npcList, null, 4)], { type: 'text/json' }))
-    // a.download = '' + window.narratorTools.sharedElements.mapId + '.json'
-    // document.body.appendChild(a)
-    // a.click()
-    // document.body.removeChild(a)
+    const a = window.document.createElement('a')
+    a.href = window.URL.createObjectURL(new window.Blob([JSON.stringify(arr, null, 2)], {type: 'text/json'}))
+    if (INTERFACE === 'NI')
+    {
+        a.download = '' + Engine.map.d.id + '.json'
+    }
+    else
+    {
+        a.download = '' + map.id + '.json'
+    }
+    document.body.appendChild(a)
+    a.click()
+    document.body.removeChild(a)
 }
 
 export function initLightManager()
@@ -54,24 +104,61 @@ export function initLightManager()
         $style.appendTo('head')
 
         const $lightManager = $(`
-<div id="nerthus-light-manager">
-
+<div id="nerthus-light-manager" class="nerthus-panel">
+    <div class="header-label-positioner">
+        <div class="header-label">
+            <div class="left-decor"></div>
+            <div class="right-decor"></div>
+            <span class="panel-name">Narzędzia światła</span>
+        </div>
+    </div>
+    <div class="close-decor">
+        <button class="close-button"></button>
+    </div>
+    <div class="background">
+        <a id="nerthus-light-manager-toggle-lights" class="button" tip="Pokaż światła">
+            <img src="${FILE_PREFIX}res/img/panel/sun.png">
+        </a>
+        <a id="nerthus-light-manager-toggle-mousemove" class="button" tip="Zablokuj chodzenie myszką">
+            <img src="${FILE_PREFIX}res/img/panel/mouse.png">
+        </a>
+        <a id="nerthus-light-manager-toggle-border" class="button" tip="Przełącz obramowanie świateł">
+            <img src="${FILE_PREFIX}res/img/panel/border.png">
+        </a>
+        <a id="nerthus-light-manager-save" class="button" tip="Pobierz dane o światłach">
+            <img src="${FILE_PREFIX}res/img/panel/download.png">
+        </a>
+    </div>
 </div>
-    `)
+    `.replace(/\n| {2}/g, ''))
+
+        const $toggleLights = $lightManager.find('#nerthus-light-manager-toggle-lights')
+        $toggleLights.click(toggleLights)
+        if (lightsOn) $toggleLights.addClass('blue')
+
+        const $toggleMouseMove = $lightManager.find('#nerthus-light-manager-toggle-mousemove')
+        $toggleMouseMove.click(toggleMouseMove)
+        console.warn('can move with mouse: ' + (hero.opt & 64))
+        if (hero.opt & 64) $toggleMouseMove.addClass('blue')
+
+        $lightManager.find('#nerthus-light-manager-toggle-border').click(toggleBorder)
+        $lightManager.find('#nerthus-light-manager-save').click(downloadLog)
+
 
         $lightManager
-            .appendTo('#centerbox2')
             .draggable({
-                start: function ()
+                start()
                 {
                     const lock = window.g ? window.g.lock : window.Engine.lock
                     lock.add('nerthus-panel-drag')
                 },
-                stop: function ()
+                stop()
                 {
                     const lock = window.g ? window.g.lock : window.Engine.lock
                     lock.remove('nerthus-panel-drag')
                 }
             })
+            .appendTo('#centerbox2')
+            .css('position', 'absolute')
     }
 }

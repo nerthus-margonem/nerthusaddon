9fe050cd55c9cee23d9559036a558ec2c6f13350
Krzysztof K <Krzysztof.Kacz@interia.pl>
2020-10-12 17:27:38 +0200

    Change from cheerio back to jsdom
    Incompatibilities were too big to have reliable tests

--- test/main.js
+++ test/main.js
@@ -1,19 +1,33 @@
-import * as cheerio from 'cheerio'
-
 function init()
 {
-    function resetWindow()
-    {
-        global.$ = cheerio.load('')
-        global.window = {}
-        // global.document = {}
-    }
+    const testHTML = `
+        <!DOCTYPE html>
+        <html lang="pl">
+        <head>
+            <title>Nerthus addon</title>
+        </head>
+        <body>
+            <div></div>
+        </body>
+        </html>
+    `;
+    const { JSDOM } = require( 'jsdom' );
+    const jsdom = new JSDOM( testHTML );
+
+    // Set window and document from jsdom
+    const { window } = jsdom;
+    const { document } = window;
+
+    // Also set global window and document before requiring jQuery
+    global.window = window;
+    global.document = document;
+
+    global.$ = global.jQuery = require( 'jquery' );
+
 
     global.Image = function () {this.src = ''}
     global.navigator = {userAgent: 'node.js'}
-    global.resetWindow = resetWindow
-
-    resetWindow()
+    global.resetWindow = init
 }
 
 init()

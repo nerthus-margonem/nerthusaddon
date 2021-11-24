5e05c44d15f127ad8231c9d7baab3b8277e10e6b
Krzysztof Kaczmarzyk <Krzysztof.Kacz@interia.pl>
2021-11-24 14:55:28 +0100

    Fixed drunkenness check

--- src/chat/drunkenness.js
+++ src/chat/drunkenness.js
@@ -35,7 +35,7 @@ function shuffleMessage(msg)
         return msg
 
     let t = []
-    switch (Math.floor(this.lvl / 10))
+    switch (Math.floor(intoxicationLvl / 10))
     {
         case 10:
         case 9:

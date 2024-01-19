637ad1182ace68a81d61f67d9913d55040741c4a
Krzysztof Kaczmarzyk <Krzysztof.Kacz@interia.pl>
2024-01-19 22:41:05 +0100

    Fix changing NPC image to animated gif when using game images

--- src/npc/npc-actions/change.js
+++ src/npc/npc-actions/change.js
@@ -10,15 +10,25 @@ export function changeGameNpc(npc)
             {
                 const img = new Image()
                 img.src = resolveUrl(npc.newUrl)
-                newNpc.sprite = img
-                Object.defineProperty(newNpc, 'sprite', {
-                    get() {return img},
-                    set() {}
-                })
+
+                if (img.src.endsWith('gif') && img.src.startsWith('https://micc.garmory-cdn.cloud/obrazki/npc/')) {
+                    newNpc.d.icon = img.src.substring(43)
+                    Engine.npcs.updateData({[newNpc.d.id]: newNpc.d})
+                }
+                else
+                {
+                    const img = new Image()
+                    img.src = resolveUrl(npc.newUrl)
+                    newNpc.sprite = img
+                    Object.defineProperty(newNpc, 'sprite', {
+                        get() {return img},
+                        set() {}
+                    })
+                }
 
                 // Run only once
                 // setTimeout so that it removes itself after all NPCs are checked by API,
-                // otherwise it would throw error
+                // otherwise it would throw an error
                 setTimeout(API.removeCallbackFromEvent.bind(API, 'newNpc', callback), 0)
             }
         }

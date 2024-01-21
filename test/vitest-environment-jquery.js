5a2b53a807ded9f1d63784fdf707c8ae3ab04675
Krzysztof Kaczmarzyk <Krzysztof.Kacz@interia.pl>
2024-01-22 00:05:03 +0100

    Add vitest environment for further tests

---
+++ test/vitest-environment-jquery.js
@@ -0,0 +1,30 @@
+import jQuery from 'jquery'
+import {JSDOM} from 'jsdom'
+
+export default {
+    name: 'jquery',
+    transformMode: 'ssr',
+    async setup()
+    {
+        const testHTML = `
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title></title>
+        </head>
+        <body></body>
+        </html>
+        `
+        const jsdom = new JSDOM(testHTML)
+        global.window = jsdom.window
+        global.Image = jsdom.window.Image
+        global.$ = global.jQuery = jQuery(window)
+
+        return {
+            teardown()
+            {
+                window.close()
+            }
+        }
+    }
+}

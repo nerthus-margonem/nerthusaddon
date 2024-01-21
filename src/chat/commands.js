35eb4554d0576a512ccaa837f997c593c8dbf01a
Krzysztof Kaczmarzyk <Krzysztof.Kacz@interia.pl>
2024-01-22 00:04:18 +0100

    Fix a crash on private message commands

--- src/chat/commands.js
+++ src/chat/commands.js
@@ -27,7 +27,7 @@ function nar1(msg)
 {
     msg.style = 'nerthus-nar1'
     msg.nick = msg.authorBusinessCard.getNick()
-    msg.authorBusinessCard = null
+    msg.authorBusinessCard.getNick = () => ''
     msg.text = msg.text.replace(/^\*nar1? /, '')
     return msg
 }
@@ -36,7 +36,7 @@ function nar2(msg)
 {
     msg.style = 'nerthus-nar2'
     msg.nick = msg.authorBusinessCard.getNick()
-    msg.authorBusinessCard = null
+    msg.authorBusinessCard.getNick = () => ''
     msg.text = msg.text.replace(/^\*nar2 /, '')
     return msg
 }
@@ -45,7 +45,7 @@ function nar3(msg)
 {
     msg.style = 'nerthus-nar3'
     msg.nick = msg.authorBusinessCard.getNick()
-    msg.authorBusinessCard = null
+    msg.authorBusinessCard.getNick = () => ''
     msg.text = msg.text.replace(/^\*nar3 /, '')
     return msg
 }
@@ -94,7 +94,7 @@ function sys(msg)
 {
     msg.style = 'nerthus-sys'
     msg.nick = msg.authorBusinessCard.getNick()
-    msg.authorBusinessCard = null
+    msg.authorBusinessCard.getNick = () => ''
     msg.text = msg.text.replace(/^\*sys /, '')
     return msg
 }
@@ -234,7 +234,7 @@ function me(msg)
 {
     msg.style = 2 // 2 is special style for regular `/me`
     msg.nick = msg.authorBusinessCard.getNick()
-    msg.authorBusinessCard = null
+    msg.authorBusinessCard.getNick = () => ''
     msg.text = msg.text.replace(/^\*me /, '')
     return msg
 }

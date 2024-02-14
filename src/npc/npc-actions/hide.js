8a55a83d58c00fa98d303210695c2f459bab7b06
Krzysztof Kaczmarzyk <Krzysztof.Kacz@interia.pl>
2024-02-15 00:44:41 +0100

    Move removing NPC to after event loop passes
    The game added additional checks and changes right after the event runs, so we need to remove NPC after those functions run. Otherwise, we would crash the game as the NPC would be deleted before those checks, which would rise type error when trying to access properties of non-existent NPC

--- src/npc/npc-actions/hide.js
+++ src/npc/npc-actions/hide.js
@@ -46,15 +46,24 @@ export function hideGameNpc(id, always)
             {
                 if (newNpc.d && newNpc.d.id === id.toString())
                 {
-                    // Remove in this fashion (instead of just calling .delete()
-                    // because we don't want to unset collisions when hiding
-                    API.callEvent('removeNpc', newNpc)
-                    if (Engine.npcs.getById(newNpc.d.id)) Engine.npcs.removeOne(newNpc.d.id)
-                    Engine.emotions.removeAllFromSourceId(newNpc.d.id)
                     // Run only once
                     // setTimeout so that it removes itself after all NPCs are checked by API,
-                    // otherwise it would throw error
-                    setTimeout(API.removeCallbackFromEvent.bind(API, 'newNpc', callback), 0)
+                    // otherwise it would throw an error
+                    setTimeout(() =>
+                    {
+                        API.removeCallbackFromEvent.bind(API, 'newNpc', callback)
+
+                        // Remove in this fashion (instead of just calling .delete())
+                        // because we don't want to unset collisions when hiding.
+                        //
+                        // We also need to remove it after the callback is executed
+                        // to fight race condition as the 'newNpc'
+                        // event is fired before all the checks and changes
+                        // for the NPC are made in the game
+                        API.callEvent('removeNpc', newNpc)
+                        if (Engine.npcs.getById(newNpc.d.id)) Engine.npcs.removeOne(newNpc.d.id)
+                        Engine.emotions.removeAllFromSourceId(newNpc.d.id)
+                    }, 0)
                 }
             }
             API.addCallbackToEvent('newNpc', callback)

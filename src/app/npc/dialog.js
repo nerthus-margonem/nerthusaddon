9c9b0bb70921ce2a688e0cffda788e0b923c7bb7
Krzysztof Kaczmarzyk <k.kaczmarzyk@interia.pl>
2025-03-16 19:50:07 +0100

    Update the way dialogues work on SI
    Handle LINE_NEXT options, disable custom scroll as now the game uses browser scroll for NPC dialogues.

--- src/app/npc/dialog.js
+++ src/app/npc/dialog.js
@@ -1,6 +1,7 @@
 import {coordsToId} from '../utility-functions'
 
-const LINE_ICON = 'icon LINE_OPTION line_option' // TODO NI/SI?
+const LINE_OPTION_ICON = 'icon LINE_OPTION line_option'
+const LINE_NEXT_ICON = 'icon LINE_NEXT line_option' // line_next is not used on NI
 const EXIT_ICON = 'icon LINE_EXIT line_exit'
 
 let dialogList = {}
@@ -36,12 +37,9 @@ export function openDialog(npcId, index)
 
 function parseReplies(dialogInstance, npcId)
 {
-    const replies = []
-
-    for (let i = 1; i < dialogInstance.length; i++)
-        replies.push(parseReply(dialogInstance[i], npcId))
-    return replies
-
+    return dialogInstance
+        .slice(1)
+        .map((reply) => parseReply(reply, npcId))
 }
 
 
@@ -57,12 +55,10 @@ function parseReply(replyRow, npcId)
     {
         reply.click = function ()
         {
-            removeScroll()
             openDialog(npcId, reply.to)
-            addScroll()
             $('#dlgin').scrollTop(0)
         }
-        reply.icon = LINE_ICON
+        reply.icon = reply.text === 'Dalej' ? LINE_NEXT_ICON : LINE_OPTION_ICON
     }
     return reply
 }
@@ -70,7 +66,10 @@ function parseReply(replyRow, npcId)
 function parseReplyRow(reply)
 {
     const replyArr = reply.split('->')
-    return {text: parsePlaceholders(replyArr[0]), to: replyArr[1]}
+    return {
+        text: parsePlaceholders(replyArr[0]).trim(),
+        to: replyArr[1].trim()
+    }
 }
 
 function parsePlaceholders(text)
@@ -81,44 +80,20 @@ function parsePlaceholders(text)
         return text.replace('#NAME', hero.nick)
 }
 
-function addScroll()
-{
-    clearInterval(g.talk.scrollCheckInterval)
-    const $dlgin = $('#dlgin')
-    g.talk.scrollCheckInterval = setInterval(function ()
-    {
-        if ($('#talkscroll').length) return
-        if ($dlgin.height() < ($('#dlgin .message').innerHeight() + $('#dlgin .replies').innerHeight()))
-        {
-            $('#dlgin').css({'margin-right': 20})
-            addScrollbar('dlgin', 490, 'talkscroll')
-        }
-    }, 100)
-    if ($dlgin.height() < ($('#dlgin .message').innerHeight() + $('#dlgin .replies').innerHeight()))
-    {
-        $dlgin.css({'margin-right': 20})
-        addScrollbar('dlgin', 490, 'talkscroll')
-    }
-}
-
-function removeScroll()
-{
-    removeScrollbar('dlgin', 'talkscroll')
-}
-
 function parseInnerDialog(npcMessage, playerReplies)
 {
     let innerDial = '<ul class="answers">'
     const repliesLen = playerReplies.length
     for (let i = 0; i < repliesLen; i++)
     {
-        let iconClass = LINE_ICON
-        if (playerReplies[i].to === 'END') iconClass = EXIT_ICON
+        const reply = playerReplies[i]
+        let iconClass = reply.text.trim() === 'Dalej' ? LINE_NEXT_ICON : LINE_OPTION_ICON
+        if (reply.to === 'END') iconClass = EXIT_ICON
 
         innerDial +=
             '<li class="answer dialogue-window-answer ' + iconClass + '">' +
             '<div class="' + iconClass + '"></div>' +
-            '<span class="answer-text">' + (i + 1) + '. ' + playerReplies[i].text + '</span>' +
+            '<span class="answer-text">' + (i + 1) + '. ' + reply.text + '</span>' +
             '</li>'
     }
     innerDial += '</ul>'
@@ -161,7 +136,6 @@ function displayDialog(npcId, npcNick, npcMessage, playerReplies)//(message, rep
         $('#dlgin .message').empty().append('<h4><b>' + npcNick + '</b></h4>' + npcMessage)
         const $replies = $('#dlgin .replies').empty()
         $replies.append(...playerReplies.map(composeReply))
-        addScroll()
         $('#dialog').show()
     }
 }
@@ -170,7 +144,6 @@ function closeDialog()
 {
     if (INTERFACE === 'NI')
     {
-        //$('.dialogue-window').removeClass('is-open')
         Engine.dialogue.finish()
         Engine.lock.remove('nerthus-dialog')
     }
@@ -179,7 +152,6 @@ function closeDialog()
         $('#dialog').hide()
         g.lock.remove('nerthus-dialog')
         map.resizeView()
-        removeScroll()
     }
 }
 

c52d2ea41b1bcdfdd436b2824b633fc767de37c6
Krzysztof Kaczmarzyk <k.kaczmarzyk@interia.pl>
2026-07-04 01:19:37 +0200

    Fix saving true/false keys in the localStorage

--- src/app/chat/hide-chat-commercials.ts
+++ src/app/chat/hide-chat-commercials.ts
@@ -1,6 +1,6 @@
 import { addCustomStyle, removeCustomStyle } from "../interface/css-manager.js";
 import { addSettingToPanel } from "../interface/panel.js";
-import { saveSetting, settings } from "../settings.js";
+import { settings } from "../settings.js";
 
 const HIDE_CHAT_COMMERCIALS_STYLE_NAME = "hide-chat-commercials";
 const HIDE_CHAT_COMMERCIALS_CSS = ".chat-COMMERCIAL-message { display: none; }";
@@ -15,7 +15,6 @@ export function initHideChatCommercials(): void {
     "Ukrywanie reklam",
     "Pokazuje lub ukrywa reklamy pojawiające się na czacie gry.",
     () => {
-      saveSetting(settings.hideChatCommercials, !settings.hideChatCommercials);
       if (settings.hideChatCommercials) {
         addCustomStyle(
           HIDE_CHAT_COMMERCIALS_STYLE_NAME,
